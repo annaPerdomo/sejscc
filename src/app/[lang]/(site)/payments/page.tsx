ffd95fdbe66c1067176fd98ab231a5646a14d670
Anna Perdomo <annamaliaperdomo@gmail.com>
2026-08-23 00:31:36 -0700

    perf: lazy-load the donation iframe

--- src/app/[lang]/(site)/payments/page.tsx
+++ src/app/[lang]/(site)/payments/page.tsx
@@ -164,6 +164,7 @@ export default async function PaymentsPage() {
             <iframe
               title={dict.payments.donateFrame}
               src={DONATION_EMBED_URL}
+              loading="lazy"
               className="h-160 w-full"
               allow="payment"
             />
